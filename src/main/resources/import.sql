5dd9d0aa0194d034bf223c41b0074c9a88102adc
Eduardo Bregaida <eduardo.bregaida@gmail.com>
2013-08-18 23:25:27 -0300

    Populado Aulas e Nome de Pista
    As aulas seguem os horarios q cada instrutor pode dar aqaquela aula,
    importante para carregar a lista de instrutores na Agenda

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,12 @@
-insert into Aluno (id, alistamentoMilitar, ativo, cpf, escolaridade, nome, rg, tituloEleitor, cht, cma, codigoANAC) values (null, 123, 'Y', 11111111111, 'SUPERIOR', 'José da Silva', 3245678, 2341, 2323, 2323, 2323);
-insert into Aluno (id, alistamentoMilitar, ativo, cpf, escolaridade, nome, rg, tituloEleitor, cht, cma, codigoANAC) values (null, 234, 'Y', 22222222222, 'MESTRADO', 'Paula Tejando', 3445599, 1321, 1123, 2553, 2001);
-insert into Aluno (id, alistamentoMilitar, ativo, cpf, escolaridade, nome, rg, tituloEleitor, cht, cma, codigoANAC) values (null, 345, 'Y', 33333333333, 'TECNICO', 'Thomás Turbando', 2422599, 1001, 1003, 1153, 1221);
-insert into Aeronave (id, apoliceSeguro, ativo, certificadoMatricula, checkList, dentel, diarioBordo, fiam, fichaPesoBalanceamento, manualVoo, marca, modelo, nsca3_5, nsca3_7) values (null, 123, 'Y', 1234, 234, 'Y', '12', 'Y', 'Y', 'Y', 'Cessna', '172', 'Y', 'Y');
-insert into Aeronave (id, apoliceSeguro, ativo, certificadoMatricula, checkList, dentel, diarioBordo, fiam, fichaPesoBalanceamento, manualVoo, marca, modelo, nsca3_5, nsca3_7) values (null, 234, 'Y', 4567, 567, 'Y', '33', 'Y', 'Y', 'Y', 'Airbus', 'A300', 'Y', 'Y');
-insert into Aula (id,materia, quantidadeHorasNecessarias, aeronave_id) values (null,'Aula Livre', 300, 1);
+insert into Aluno (id, alistamentoMilitar, ativo, cpf, escolaridade, nome, rg, tituloEleitor, cht, cma, codigoANAC, nomePista) values (null, 123, 'Y', 11111111111, '1', 'José da Silva', 3245678, 2341, 2323, 2323, 2323, 'Zé');
+insert into Aluno (id, alistamentoMilitar, ativo, cpf, escolaridade, nome, rg, tituloEleitor, cht, cma, codigoANAC, nomePista) values (null, 234, 'Y', 22222222222, '2', 'Paula Tejando', 3445599, 1321, 1123, 2553, 2001, 'Paulinha Asa Fixa');
+insert into Aluno (id, alistamentoMilitar, ativo, cpf, escolaridade, nome, rg, tituloEleitor, cht, cma, codigoANAC, nomePista) values (null, 345, 'Y', 33333333333, '3', 'Thomás Turbando', 2422599, 1001, 1003, 1153, 1221, 'Turbando');
+insert into Aeronave (id, apoliceSeguro, ativo, certificadoMatricula, checkList, dentel, diarioBordo, fiam, fichaPesoBalanceamento, manualVoo, marca, modelo, nsca3_5, nsca3_7) values (null, 123, 'Y', 'PT-NXZ', 234, 'Y', '12', 'Y', 'Y', 'Y', 'Cessna', '172', 'Y', 'Y');
+insert into Aeronave (id, apoliceSeguro, ativo, certificadoMatricula, checkList, dentel, diarioBordo, fiam, fichaPesoBalanceamento, manualVoo, marca, modelo, nsca3_5, nsca3_7) values (null, 234, 'Y', 'PR-MRG', 567, 'Y', '33', 'Y', 'Y', 'Y', 'Airbus', 'A300', 'Y', 'Y');
+insert into Aeronave (id, apoliceSeguro, ativo, certificadoMatricula, checkList, dentel, diarioBordo, fiam, fichaPesoBalanceamento, manualVoo, marca, modelo, nsca3_5, nsca3_7) values (null, 234, 'Y', 'PT-IZQ', 567, 'Y', '33', 'Y', 'Y', 'Y', 'Piper', 'Cherokee', 'Y', 'Y');
+insert into Aula (id,materia, quantidadeHorasNecessarias, aeronave_id) values (null,'Manobras Altas', 50, 1);
+insert into Aula (id,materia, quantidadeHorasNecessarias, aeronave_id) values (null,'TGL', 200, 1);
+insert into Aula (id,materia, quantidadeHorasNecessarias, aeronave_id) values (null,'Aperfeiçoamento', 250, 2);
+insert into Aula (id,materia, quantidadeHorasNecessarias, aeronave_id) values (null,'INVA', 300, 3);
+insert into Aula (id,materia, quantidadeHorasNecessarias, aeronave_id) values (null,'Acrobática', 500, 3);
 insert into Instrutor (id,nome, cma, cht,codigoANAC,ativo) values (null,'Santos Dumont', 300123, 12345,145678,'Y');
