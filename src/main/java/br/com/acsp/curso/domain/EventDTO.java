ff733a5dde27d8e47be7b8aefa5223ded5cfa5b2
Christian Reichel <reichel.christian@gmail.com>
2013-09-03 23:44:38 +0100

    Mal ae

---
+++ src/main/java/br/com/acsp/curso/domain/EventDTO.java
@@ -0,0 +1,82 @@
+package br.com.acsp.curso.domain;
+
+import org.apache.commons.lang.time.DateUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * User: Christian Reichel
+ * Date: 9/3/13
+ * Time: 10:27 PM
+ */
+public class EventDTO {
+
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    private String title;
+    private String start;
+    private String end;
+    private boolean allDay = false;
+    private long id;
+    private final SimpleDateFormat sdf = new SimpleDateFormat(DATETIME_FORMAT);
+
+    public EventDTO() {
+    }
+
+    public EventDTO(Agenda agenda) {
+        this.title = agenda.getAluno().getNomePista();
+        this.setStart(agenda.getDataReserva());
+        this.setEnd(DateUtils.addHours(agenda.getDataReserva(), agenda.getQtdeHoras()));
+        this.id = agenda.getId();
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public void setStart(Date start) {
+        this.start = sdf.format(start);
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = sdf.format(end);
+    }
+
+    public boolean isAllDay() {
+        return allDay;
+    }
+
+    public void setAllDay(boolean allDay) {
+        this.allDay = allDay;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
