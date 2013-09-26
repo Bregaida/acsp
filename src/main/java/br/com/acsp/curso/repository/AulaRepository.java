b08431645071adc8931b335ece048c52c538c189
Eduardo Bregaida <eduardo.bregaida@gmail.com>
2013-09-26 01:01:50 -0300

    Todos os selects estão ok
    Os selects dos combos aninhados estão funcionando no banco, necessita
    implementação no Spring Data.
    Lista de:
    Aeronaves homologadas por aluno
    Instrutor por Aeronave e Aula
    Instrutores Somente por Aeronaves
    Aulas Por Aeronaves

--- src/main/java/br/com/acsp/curso/repository/AulaRepository.java
+++ src/main/java/br/com/acsp/curso/repository/AulaRepository.java
@@ -3,11 +3,12 @@
  */
 package br.com.acsp.curso.repository;
 
-import br.com.acsp.curso.domain.Aula;
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
+import br.com.acsp.curso.domain.Aula;
 
 /**
  * @author eduardobregaida
@@ -16,4 +17,9 @@ public interface AulaRepository extends JpaRepository<Aula, Long> {
 
     @Query("select a from Aula a order by a.materia")
     Collection<Aula> listarOrdenadoPorMateria();
+
+    // select au.materia, ae.id from aula_aeronave aa left join Aula au on
+    // (au.id = aa.aula_id) left join Aeronave ae on (ae.id = aa.aeronave_id)
+    // where ae.id= :aeronaveId;
+    Collection<Aula> listarAulasPorAeronave();
 }
--- src/main/java/br/com/acsp/curso/repository/InstrutorRepository.java
+++ src/main/java/br/com/acsp/curso/repository/InstrutorRepository.java
@@ -15,9 +15,32 @@ import br.com.acsp.curso.domain.Instrutor;
  */
 public interface InstrutorRepository extends JpaRepository<Instrutor, Long> {
 
-	@Query("select a from Instrutor a order by a.nome")
-	Collection<Instrutor> listarOrdenadoPorNome();
+    @Query("select a from Instrutor a order by a.nome")
+    Collection<Instrutor> listarOrdenadoPorNome();
 
     @Query("select a from Instrutor a where a.ativo <> 'N' order by a.nome ")
     Collection<Instrutor> listarAtivos();
+
+    // select p.nome
+    // from Pessoa_Aeronave pa
+    // left join Pessoa p on (p.id = pa.Pessoa_id)
+    // left join Aeronave ae on (ae.id = pa.aeronaves_id)
+    // left join Aula au on (p.quantidadeHorasMinistrada >=
+    // au.quantidadeHorasNecessarias)
+    // where p.PESSOA_TYPE='INSTRUTOR_PILOTO'
+    // #and p.ativo = 'true'
+    // and au.id = :aula
+    // and ae.id= :aeronave;
+    Collection<Instrutor> listarInstrutoresPorAeronavePorAula();
+
+    // select p.nome
+    // from Pessoa_Aeronave pa
+    // left join Pessoa p on (p.id = pa.Pessoa_id)
+    // left join Aeronave ae on (ae.id = pa.aeronaves_id)
+    // left join Aula au on (p.quantidadeHorasMinistrada >=
+    // au.quantidadeHorasNecessarias)
+    // where p.PESSOA_TYPE='INSTRUTOR_PILOTO'
+    // #and p.ativo = 'true'
+    // and ae.id= :aeronave;
+    Collection<Instrutor> listarInstrutoresPorAeronaveSemAula();
 }
